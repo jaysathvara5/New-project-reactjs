d145bcd35cccb16ac4cb3c706a0af9f2302a0f71
Jay Sathvara <jay.sathwara@kitchen365.com>
2024-08-29 18:21:22 +0530

    Todo List Project

--- src/App.js
+++ src/App.js
@@ -1,26 +1,28 @@
 import logo from './logo.svg';
 import './App.css';
+import About from "./screens/About";
+import Home from "./screens/Home";
+import Registerform from './screens/Reginsterform';
+import Userdata from './screens/Userdata';
+import Todos from './screens/Todos';
 
+// function Mybutton(){
+//   return(
+//     <button className='btn'>CLick Here</button>
+//   );
+// }
 
-function Mybutton(){
-  return(
-    <button className='btn'>CLick Here</button>
-  );
-}
-
-function Header(){
-  return(
-    <h1 className="h1">This Title</h1>
-  )
-}
+// function Header(){
+//   return(
+//     <h1 className="h1">This Title</h1>
+//   )
+// }
  
 function App() {
   return (
-    <div className="App">
-      <h1>hello world!</h1>
-      <Mybutton />
-      <Header />
-    </div>
+  // <Registerform />
+  // <Userdata />
+  <Todos />
   );
 }
 
