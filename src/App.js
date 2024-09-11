2db85868a74a0119c1b4bd61fdd89cb178fd08e2
Jay Sathvara <jay.sathwara@kitchen365.com>
2024-09-11 11:54:08 +0530

    select box

--- src/App.js
+++ src/App.js
@@ -1,11 +1,12 @@
 import './App.css';
+import Selectbox from './components/Selectbox';
 import Crudopration from './screens/Crudopration';
 
 function App() {
 return(
   <>
     <Crudopration />
-    
+    <Selectbox />
   </>
 );  
   
---
+++ src/components/Selectbox.js
@@ -0,0 +1,16 @@
+import React from 'react'
+
+function Selectbox() {
+  return (
+    <div>
+      <select>
+        <option >select</option>
+        <option >Apple</option>
+        <option >Banana</option>
+        <option >Mango</option>
+      </select>
+    </div>
+  )
+}
+
+export default Selectbox
