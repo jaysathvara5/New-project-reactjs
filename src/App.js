7951caabc38e4b327658330f93d2b322f27887a4
Jay Sathvara <jay.sathwara@kitchen365.com>
2024-09-11 12:15:43 +0530

    sd

--- src/App.js
+++ src/App.js
@@ -2,10 +2,6 @@ import './App.css';
 import Selectbox from './components/Selectbox';
 import Crudopration from './screens/Crudopration';
 
-<<<<<<< HEAD
-//hellod
-=======
->>>>>>> 2db85868a74a0119c1b4bd61fdd89cb178fd08e2
 function App() {
 return(
   <>
